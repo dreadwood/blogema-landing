f2d7c91a3f0ebb3d4bc577ff0b67f0151b917716
srg <skabassjob@gmail.com>
2024-09-16 14:43:08 +0400

    init:

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import svgSprite from "astro-svg-sprite";
+
+// https://astro.build/config
+export default defineConfig({
+  scopedStyleStrategy: 'where',
+  integrations: [svgSprite({
+    mode: "verbose",
+      include: [
+        "./src/assets/icons",
+      ],
+      emitFile: {
+        compress: "standard",
+        path: "assets",
+      }
+  })],
+});
